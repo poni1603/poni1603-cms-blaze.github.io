c84b145226473dbfbcf95434cba2be108f836485
poni1603 <114936407+poni1603@users.noreply.github.com>
2022-11-04 08:28:06 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+
+function openRightMenu() {
+  document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
+}
